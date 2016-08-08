3d2a294221d22d317c978ce3d51f555f9264b921
commaweed <tjjenk2@yahoo.com>
2016-08-08 18:53:12 -0400

    modified to hide the password, etc.

--- src/main/java/fezzik/domain/User.java
+++ src/main/java/fezzik/domain/User.java
@@ -8,25 +8,79 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.data.annotation.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = -2751305641698104649L;
 	
 	@Id
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String password;
+    private final String uuid;
+    
+    @JsonIgnore
+    private final String password;
+    
+    private final String firstName;
+    private final String lastName;
 
+    /**
+     * Default constructor used by spring-data.
+     */
     public User() {
+    	this.uuid = null;
+    	this.firstName = null;
+    	this.lastName = null;
+    	this.password = null;
     }
 
-    public User(String firstName, String lastName) {
+    /**
+     * Initialize with the required fields.
+     * @param 
+     * @param firstName The first name of the user.
+     * @param lastName The last name of the user.
+     * @param password The encrypted password of the user.
+     */
+    public User(
+    	String uuid,
+		String firstName, 
+		String lastName,
+		String password
+    ) {
+    	this.uuid = uuid;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.password = password;
     }
-    
-    /**
+
+	/**
+	 * @return the uuid
+	 */
+	public String getUuid() {
+		return uuid;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
      * {@inheritDoc}
      */
     @Override
