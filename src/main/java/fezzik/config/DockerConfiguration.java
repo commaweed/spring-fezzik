e56a1a487c827e5828e2c531ad78be3d5e85fd9b
ets-mlb <mattbrake@elegantts.com>
2016-08-08 18:16:15 -0400

    updated profiles

--- src/main/java/fezzik/config/DockerConfiguration.java
+++ src/main/java/fezzik/config/DockerConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
-public class ProductionConfiguration {
+@Profile("docker")
+public class DockerConfiguration {
 
 }
 
